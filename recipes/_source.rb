8824adcdc560b6469f8030fb3d7b65d508e577a7
Stephan Renatus <s.renatus@x-ion.de>
2014-01-08 18:23:48 +0100

    no longer needed \o/

--- recipes/_source.rb
+++ recipes/_source.rb
@@ -23,13 +23,6 @@ end
   end
 end
 
-execute 'fix Gemfile for archive gem bugfix' do
-  command "sed -i s#^gem 'archive'$#gem 'archive', git: 'git://github.com/erikh/archive.git', branch: 'd1f7e72e18aefb591af2d638e1ccd7de51d2f308'# #{node[:razor][:home]}/Gemfile"
-  action :nothing
-  subscribes :run, "git[#{node[:razor][:home]}]", :immediately
-end
-
-
 include_recipe 'razor::_config'
 
 [
