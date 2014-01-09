c172dac0e3257f900bcafa7ff6a162b9f81ac00f
Stephan Renatus <s.renatus@x-ion.de>
2014-01-09 17:06:22 +0100

    make db port configurable

--- recipes/_config.rb
+++ recipes/_config.rb
@@ -2,6 +2,7 @@ template "#{node[:razor][:home]}/config.yaml" do
   source 'config.yaml.erb'
   variables(
     db_host: node[:razor][:database][:host],
+    db_port: node[:razor][:database][:port],
     repo_store_root: node[:razor][:repo_store_root]
   )
 end
