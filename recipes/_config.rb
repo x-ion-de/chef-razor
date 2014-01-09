2978328133c2792cc85674f6bf487945aa1b35d6
Stephan Renatus <s.renatus@x-ion.de>
2014-01-09 13:40:03 +0100

    update config.yaml.erb

--- recipes/_config.rb
+++ recipes/_config.rb
@@ -2,7 +2,6 @@ template "#{node[:razor][:home]}/config.yaml" do
   source 'config.yaml.erb'
   variables(
     db_host: node[:razor][:database][:host],
-    repo_service_uri: "http://#{node[:fqdn]}/razor/repo",
     repo_store_root: node[:razor][:repo_store_root]
   )
 end
